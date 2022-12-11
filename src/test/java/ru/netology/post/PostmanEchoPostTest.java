a7123e4eb15969817074bb9bc5b5396eb16f28e3
sanyaminkin <sanyaminkin@gmail.com>
2022-12-12 01:18:41 +0300

    PostmanEchoPostTest.java added

---
+++ src/test/java/ru/netology/post/PostmanEchoPostTest.java
@@ -0,0 +1,26 @@
+package ru.netology.post;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoPostTest {
+    @Test
+    void shouldSendUserData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
